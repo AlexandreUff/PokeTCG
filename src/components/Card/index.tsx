ad386cf221a8356e12ce8b1b88a15f41744f910b
AlexandreUff <alexandre.magno.wrk@gmail.com>
2025-01-20 12:12:07 -0300

    refac: styles adjusts

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -2,14 +2,17 @@ import styles from './index.module.scss'
 import { Picture } from '../Icons'
 
 export default function Card(){
+
+    const pokemonName = "Pikachu"
+
     return (
         <article className={styles.card}>
-            <section className={styles['card-main']}>
+            <section title={pokemonName} className={styles['card-main']}>
                 <Picture className={styles['no-picture']} />
             </section>
             <section className={styles['card-info']}>
                 <div className={styles['title-area']}>
-                    <p className={styles['main-title']}>Bulba</p>
+                    <h2 className={styles['main-title']}>{pokemonName}</h2>
                     <p>Tipo planta</p>
                 </div>
                 <div className={styles.frequency}>
