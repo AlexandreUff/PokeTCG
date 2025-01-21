cf8c967d78de2e3984bad250fd681965932085c9
AlexandreUff <alexandre.magno.wrk@gmail.com>
2025-01-20 23:59:34 -0300

    feat: add X icon

--- src/components/Icons/index.tsx
+++ src/components/Icons/index.tsx
@@ -13,3 +13,7 @@ export const Previous = ({className} : IconProps) => {
 export const Next = ({className} : IconProps) => {
     return <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className={className ?? ''}><path d="m9 18 6-6-6-6"/></svg>
 }
+
+export const X = ({className} : IconProps) => {
+    return <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className={className ?? ''}><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg>
+}
