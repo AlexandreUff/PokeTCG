1bcf5ef7434378423434d2213c9c3b1f11bcae81
AlexandreUff <alexandre.magno.wrk@gmail.com>
2025-01-21 15:22:25 -0300

    feat: create Filter comp

---
+++ src/components/Filter/index.tsx
@@ -0,0 +1,9 @@
+import styles from "./index.module.scss"
+
+export default function Filter(){
+    return (
+        <div className={styles['filter-content']}>
+            FILTER
+        </div>
+    )
+}
