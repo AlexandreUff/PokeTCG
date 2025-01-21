21f0918b275626248968cf784637b391829751b2
AlexandreUff <alexandre.magno.wrk@gmail.com>
2025-01-21 19:41:56 -0300

    feat: wip

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -2,7 +2,7 @@ import styles from './index.module.scss'
 import { Picture } from '../Icons'
 import { useContext } from 'react'
 import { ModalContext } from '../../provider/modal'
-import { ICard } from '../../types/card-types'
+import { ICard, ITypes } from '../../types/card-types'
 
 interface CardProps {
     data: ICard;
@@ -14,19 +14,30 @@ export default function Card({data}: CardProps){
 
     console.log('Chegaram aqui', data)
 
+    function listTypesAsString(types: ITypes[]): string{
+        let text = ""
+        types.forEach((eachType: string, i: number) => {
+            text = text + `${eachType.replace('-', ' ')}${i !== types.length - 1 ? ', ' : ''}`
+        })
+        
+        return text
+    }
+
     return (
         <article className={styles.card} onClick={() => openModal("NOVO", <p>IRON</p>)}>
             <section title={data.name} className={styles['card-main']}>
-                <Picture className={styles['no-picture']} />
+                {data.images ? <img src={data.images.small} className={styles['card-image']} /> : (
+                    <Picture className={styles['no-picture']} />
+                )}
             </section>
             <section className={styles['card-info']}>
                 <div className={styles['title-area']}>
                     <h2 className={styles['main-title']}>{data.name}</h2>
-                    <p>Tipo planta</p>
+                    <p>Tipo: {listTypesAsString(data.types)}</p>
                 </div>
                 <div className={styles.frequency}>
                     <Picture className={styles['type-of-frequency']} />
-                    <p>Comum</p>
+                    <p>{data.rarity}</p>
                 </div>
             </section>
         </article>
--- src/types/card-types.ts
+++ src/types/card-types.ts
@@ -134,6 +134,58 @@ interface CardMarket {
   prices: CardMarketPrices;
 }
 
+export enum ITypes {
+  COLORLESS = "Colorless",
+  DARKNESS = "Darkness",
+  DRAGON = "Dragon",
+  FAIRY = "Fairy",
+  FIGHTING = "Fighting",
+  FIRE = "Fire",
+  GRASS = "Grass",
+  LIGHTNING = "Lightning",
+  METAL = "Metal",
+  PSYCHIC = "Psychic",
+  WATE = "Water",
+}
+
+enum IRarity {
+  ACE_SPEC_RARE = "ACE SPEC Rare",
+  AMAZING_RARE = "Amazing Rare",
+  CLASSIC_COLLECTION = "Classic Collection",
+  COMMON = "Common",
+  DOUBLE_RARE = "Double Rare",
+  HYPER_RARE = "Hyper Rare",
+  ILLUSTRATION_RARE = "Illustration Rare",
+  LEGEND = "LEGEND",
+  PROMO = "Promo",
+  RADIANT_RARE = "Radiant Rare",
+  RARE = "Rare",
+  RARE_ACE = "Rare ACE",
+  RARE_BREAK = "Rare BREAK",
+  RARE_HOLO = "Rare Holo",
+  RARE_HOLO_EX = "Rare Holo EX",
+  RARE_HOLO_GX = "Rare Holo GX",
+  RARE_HOLO_LV_X = "Rare Holo LV.X",
+  RARE_HOLO_STAR = "Rare Holo Star",
+  RARE_HOLO_V = "Rare Holo V",
+  RARE_HOLO_VMAX = "Rare Holo VMAX",
+  RARE_HOLO_VSTAR = "Rare Holo VSTAR",
+  RARE_PRIME = "Rare Prime",
+  RARE_PRISM_STAR = "Rare Prism Star",
+  RARE_RAINBOW = "Rare Rainbow",
+  RARE_SECRET = "Rare Secret",
+  RARE_SHINING = "Rare Shining",
+  RARE_SHINY = "Rare Shiny",
+  RARE_SHINY_GX = "Rare Shiny GX",
+  RARE_ULTRA = "Rare Ultra",
+  SHINY_RARE = "Shiny Rare",
+  SHINY_ULTRA_RARE = "Shiny Ultra Rare",
+  SPECIAL_ILLUSTRATION_RARE = "Special Illustration Rare",
+  TRAINER_GALLERY_RARE_HOLO = "Trainer Gallery Rare Holo",
+  ULTRA_RARE = "Ultra Rare",
+  UNCOMMO = "Uncommon",
+}
+
 export interface ICard {
   id: string;
   name: string;
@@ -141,7 +193,7 @@ export interface ICard {
   subtypes: string[];
   level: string;
   hp: string;
-  types: string[];
+  types: ITypes[];
   evolvesTo: string[];
   attacks: Attack[];
   weaknesses: Weakness[];
@@ -150,7 +202,7 @@ export interface ICard {
   set: Set;
   number: string;
   artist: string;
-  rarity: string;
+  rarity: IRarity;
   flavorText: string;
   nationalPokedexNumbers: number[];
   legalities: {
