239682874b547e188ba98e2b9a27015f54ae7921
AlexandreUff <alexandre.magno.wrk@gmail.com>
2025-01-21 00:00:01 -0300

    feat: create close Modal button

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,6 +1,7 @@
 import { useContext } from "react"
 import styles from "./index.module.scss"
 import { ModalContext } from "../../provider/modal"
+import { X } from "../Icons"
 
 export default function Modal(){
 
@@ -11,6 +12,7 @@ export default function Modal(){
             <div className={styles['modal-container']} onClick={e => e.stopPropagation()}>
                 <header className={styles['modal-header']}>
                     Teste
+                    <button className={styles['modal-close-button']} onClick={toggleOpenModal} title="Fechar"><X/></button>
                 </header>
             </div>
         </div>
