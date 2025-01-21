fe9701bb3af87ac417e89d05c283d42c35227b5c
AlexandreUff <alexandre.magno.wrk@gmail.com>
2025-01-21 20:24:56 -0300

    feat: add operation to SearchBar comp

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,11 +1,27 @@
 import styles from "./index.module.scss"
 import { Search } from "../Icons";
+import { ChangeEvent, useState } from "react";
 
-export default function SearchBar(){
+interface SearchBarProps {
+    getTerm: (term: string) => void;
+}
+
+export default function SearchBar({ getTerm }:SearchBarProps){
+
+    const [searchTerm, setSearchTerm] = useState<string>("");
+
+    function onChangeTerm(event: ChangeEvent<HTMLInputElement>){
+        setSearchTerm(event.target.value);
+    }
+
+    function handleTerm(){
+        getTerm(searchTerm ? `q=name:${searchTerm} `: "");
+    }
+
     return (
         <div className={styles["search-outline"]}>
-            <input type="text" placeholder="Pesquise um Pokemon" className={styles["search-input"]} />
-            <button className={styles["search-button"]} title="Pesquisar"><Search /></button>
+            <input type="text" placeholder="Pesquise um Pokemon" className={styles["search-input"]} value={searchTerm} onChange={onChangeTerm} />
+            <button className={styles["search-button"]} title="Pesquisar" onClick={handleTerm}><Search /></button>
         </div>
     )
 }
