9f19f7dde2dc54ded2789dfc6451b61df669c9c4
AlexandreUff <alexandre.magno.wrk@gmail.com>
2025-01-22 12:43:36 -0300

    feat: wip to Filter component

--- src/components/Filter/index.tsx
+++ src/components/Filter/index.tsx
@@ -1,10 +1,58 @@
+import { useContext, useEffect, useState } from "react"
 import { FilterList } from "../Icons"
 import styles from "./index.module.scss"
+import { ModalContext } from "../../provider/modal"
+import { FiltersService } from "../../services/filters-service"
+
+interface FilterFieldsStructure {
+    label: string,
+    selected: boolean
+}
+
+interface FilterType {
+ name: string,
+ fields: FilterFieldsStructure[]
+}
 
 export default function Filter(){
+
+    const { openModal } = useContext(ModalContext);
+
+    const [filter, setFilter] = useState<FilterType[]>([])
+
+    const filtersNamesList = [/* 'sets',  */'rarities', 'types', 'subtypes', 'supertypes']
+
+    async function getFilterLabelsByType(filterName: string){
+        const result = await FiltersService.get(filterName)
+        return result.data
+    }
+
+    async function createFiltersStructures(){
+
+        const allFilters: FilterType[] = []
+
+        filtersNamesList.forEach(async filterName => {
+            const filtersLabels = await getFilterLabelsByType(filterName);
+            const filterType: FilterType = {
+                name: filterName,
+                fields: [... filtersLabels]
+            }
+
+            console.log('filterType', filterType)
+
+            allFilters.push(filterType)
+        })
+
+        console.log('filtreiros', allFilters)
+    }
+
+    useEffect(() => {
+        createFiltersStructures()
+    }, [])
+
     return (
         <div className={styles['filter-content']}>
-            <button><FilterList /> Filtros</button>
+            <button onClick={() => openModal('Filtros', <b>RÁ</b>)}><FilterList /> Filtros</button>
             <section className={styles['filters-selected']}>
                 Teste
             </section>
