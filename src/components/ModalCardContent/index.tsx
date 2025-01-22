decb3934caf6cdb9cdbbcf7f95eee5ab43d5e4e2
AlexandreUff <alexandre.magno.wrk@gmail.com>
2025-01-21 23:00:53 -0300

    feat: wip modal datas

--- src/components/ModalCardContent/index.tsx
+++ src/components/ModalCardContent/index.tsx
@@ -5,7 +5,31 @@ export default function ModalCardContent({data}: CardProps){
     return (
         <div className={styles.content}>
             <img src={data.images.large} alt={`Imagem de ${data.name}`} className={styles.image} />
-            <p>{data.flavorText ?? <i>"Não há descrição disponpivel para este Pokemon."</i>}</p>
+            <p className={styles['pokemon-description']}>{data.flavorText ?? <i>"Não há descrição disponpivel para este Pokemon."</i>}</p>
+            <b>Attacks:</b>
+            <section className={styles['attack-content']}>
+                {data.attacks.map(attack => {
+                    return (
+                        <div className={styles["attack-info-content"]}>
+                            <p><span className={styles["attack-name"]}>{attack.name}</span> | <span className={styles["attack-damage"]}>{attack.damage}</span></p>
+                            <p className={styles['attack-description']}>{attack.text}</p>
+                        </div>
+                    )
+                })}
+            </section>
+            <b>Weakness:</b>
+            <section className={styles['weakness-content']}>
+                {data.weaknesses.map(weakness => {
+                    return (
+                        <div className={styles["weakness-pill"]}>
+                            <p>
+                            {weakness.type} | {weakness.value}
+
+                            </p>
+                        </div>
+                    )
+                })}
+            </section>
         </div>
     )
 }
